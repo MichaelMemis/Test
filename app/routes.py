4300a3fdbecdce530e6a97019e03d1ed74cedb34
Billy Winters <wwinters@ithaca.edu>
2021-11-22 16:12:43 -0500

    updated models and routes

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from app.models import User, Dish, Restaurant, Review, Vote
 
 
 @app.route('/')
