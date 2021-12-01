1ae169caf2825f0d593c56db59a830d6183dad0b
Billy Winters <wwinters@ithaca.edu>
2021-12-01 18:42:35 -0500

    progress on user and posts

--- app/routes.py
+++ app/routes.py
@@ -124,14 +124,14 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+    reviews = user.posts.order_by(Review.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
+    next_url = url_for('user', username=user.username, page=reviews.next_num) \
+        if reviews.has_next else None
+    prev_url = url_for('user', username=user.username, page=reviews.prev_num) \
+        if reviews.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts.items,
+    return render_template('user.html', user=user, posts=reviews.items,
                            next_url=next_url, prev_url=prev_url, form=form)
 
 
--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -8,3 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 5
